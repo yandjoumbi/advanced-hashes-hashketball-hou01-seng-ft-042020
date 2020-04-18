9e14ff9b5a4b0dd1153c782efa947e6a24d9e4ef
Yannick Djoumbi <ydjoumbi@gmail.com>
2020-04-18 01:16:43 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -205,15 +205,13 @@ arr
 end
 
 
-
 def player_stats(name)
-
   game_hash.each do |team, team_info|
-
     #binding.pry
-    team_info[:players].each do |player_hash|
+    #team_info[:players].each do |player_hash|
       #binding.pry
-      if player_hash[:player_name] == name
+      if team_info[:players].each do |player_hash|
+        player_hash[:player_name] == name
         binding.pry
         return player_hash.select {|k,v| k !=:player_name}
         #binding.pry
